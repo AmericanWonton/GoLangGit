1dd038d9de7f733edda3466e1f9f39d54fde6d9b
AmericanWonton <jbkeller0303@gmail.com>
2019-07-21 11:18:56 -0500

    Finished the channels, working on the error handleing.

--- testFolder/channels.go
+++ testFolder/channels.go
@@ -152,3 +152,129 @@ for {
 	}
 }
 }
+
+//Fan in
+/////////////////////////////////////////////
+/*
+Sometimes, you want your work to "fan out"; as in, go  do other stuff
+and find different results. Then, you want to "fan in", all those found
+results/answers into one channel in go.
+*/
+
+even := make(chan int)
+odd := make(chan int)
+fanin := make(chan int)
+
+go send(even, odd)
+
+go recieve(even, odd, fanin)
+
+for v := range fanin {
+	fmt.Println(v)
+}
+
+fmt.Println("About to exit...")
+
+//send channel
+func send(even, odd chan<- int) {
+	for i := 0; i < 10; i++ {
+		if i%2 == 0 {
+			even <- i
+		} else {
+			odd <- i
+		}
+	}
+	close(even)
+	close(odd)
+}
+
+//recieve channel
+func recieve(even, odd <-chan int, fanin chan<- int) {
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		for v := range even {
+			fanin <- v
+		}
+		wg.Done()
+	}()
+
+	go func() {
+		for v := range odd {
+			fanin <- v
+		}
+		wg.Done()
+	}()
+
+	wg.Wait()
+	close(fanin)
+}
+
+//Fan Out
+/////////////////////////////////////////////
+
+c1 := make(chan int)
+c2 := make(chan int)
+
+go populate(c1)
+
+go fanOutIn(c1, c2)
+
+for v := range c2 {
+	fmt.Println(v)
+}
+
+fmt.Println("About to exit...")
+
+func populate(c chan int) {
+	for i := 0; i < 100; i++ {
+		c <- i
+	}
+	close(c)
+}
+
+func fanOutIn(c1, c2 chan int) {
+	var wg sync.WaitGroup
+	for v := range c1 {
+		wg.Add(1)
+		go func(v2 int) {
+			c2 <- timeConsumingWork(v2)
+			wg.Done()
+		}(v)
+	}
+	wg.Wait()
+	close(c2)
+}
+
+func timeConsumingWork(n int) int {
+	time.Sleep(time.Microsecond * time.Duration(rand.Intn(500)))
+	return n + rand.Intn(1000) //Random num between 0 -1000
+}
+
+//Context
+/////////////////////////////////////////////
+
+/*
+Helps shut down GoRoutines that are still running.
+*/
+
+//Note to self, this is hard...fuck
+
+ctx := context.Background() //Context initialization.
+
+fmt.Println("context: \t", ctx)
+fmt.Println("context err: \t", ctx.Err())
+fmt.Printf("context type: \t%T\n", ctx)
+fmt.Println("----------")
+
+ctx, _ = context.WithCancel(ctx)
+
+fmt.Println("context: \t", ctx)
+fmt.Println("context err: \t", ctx.Err())
+fmt.Printf("context type: \t%T\n", ctx)
+fmt.Println("----------")
+
+
+
+
